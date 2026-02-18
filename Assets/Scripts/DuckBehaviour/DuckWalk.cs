01263e5d1dc8b08ea461c84085b5115738ee0105
Sunil Parab <sunilparab2023@gmail.com>
2026-02-18 13:26:15 -0500

    Updates for duck movement

--- Assets/Scripts/DuckBehaviour/DuckWalk.cs
+++ Assets/Scripts/DuckBehaviour/DuckWalk.cs
@@ -3,30 +3,35 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 [RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class DuckWalk : MonoBehaviour
 {
 
     CircleCollider2D col;
     SpriteRenderer sprite;
+    Rigidbody2D rb;
 
     [SerializeField] float speed;
     [SerializeField] Vector2 direction;
+    [SerializeField] float randomBounceOffset;
 
     void Awake()
     {
         col = GetComponent<CircleCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Start()
     {
         if (direction == Vector2.zero)
         {
-            direction = UnityEngine.Random.insideUnitCircle.normalized;
+            ChangeDirection(UnityEngine.Random.insideUnitCircle.normalized);
         }
         else
         {
-            direction.Normalize();
+            ChangeDirection(direction.normalized);
         }
     }
 
@@ -39,26 +44,59 @@ public class DuckWalk : MonoBehaviour
     {
         for (int i = 0; i < 100; i++) { //100 attempt limit to prevent infinite loop
 
-            RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position+direction*col.radius, direction, distance, LayerMask.GetMask("Floor"));
+            //Wall hits
+            RaycastHit2D wallHit = Physics2D.Raycast((Vector2)transform.position+direction, direction, distance, LayerMask.GetMask("Floor"));
 
-            if (hit)
+            if (wallHit)
             {
-                transform.Translate(hit.distance * direction);
-                distance -= hit.distance;
-                direction = Vector2.Reflect(direction, hit.normal);
-                sprite.flipX = direction.x > 0;
+                transform.Translate(wallHit.distance * direction);
+                distance -= wallHit.distance;
+                WallBounce(Vector2.Reflect(direction, wallHit.normal));
+                continue;
             }
-            else
+
+            //Duck hits
+            RaycastHit2D[] duckHits = Physics2D.RaycastAll((Vector2)transform.position+direction, direction, distance, LayerMask.GetMask("Ducks"));
+
+            foreach (RaycastHit2D duckHit in duckHits)
             {
-                transform.Translate(direction * distance);
-                return;
+                if (duckHit.collider != col)
+                {
+                    Vector3 otherPos = duckHit.collider.transform.position;
+                    duckHit.collider.GetComponent<DuckWalk>().DuckBounce(transform.position);
+                    DuckBounce(otherPos);
+                    break;
+                }
             }
+            
+            //No hits
+            transform.Translate(direction * distance);
+            return;
+            
         }
     }
 
     public void Lure(Vector2 positon)
     {
-        direction = (positon - (Vector2)transform.position).normalized;
+        ChangeDirection(direction = (positon - (Vector2)transform.position).normalized);
+    }
+
+    public void DuckBounce(Vector3 other)
+    {
+        ChangeDirection((transform.position - other).normalized);
+    }
+
+    void WallBounce(Vector2 targetDirection)
+    {
+        Vector2 newDirection = Quaternion.AngleAxis(UnityEngine.Random.Range(-randomBounceOffset,randomBounceOffset),Vector3.forward) * targetDirection;
+
+        ChangeDirection(newDirection);
+    }
+
+    void ChangeDirection(Vector2 newDirection)
+    {
+        direction = newDirection;
+        sprite.flipX = direction.x > 0;
     }
 
 }
