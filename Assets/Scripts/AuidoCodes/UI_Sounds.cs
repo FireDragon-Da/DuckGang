31062bc1fc3a6a908448fc62338a949b12db6dfc
Yousquid <ly2456@nyu.edu>
2026-02-26 08:49:41 +0800

    Add_walking_sound

--- Assets/Scripts/AuidoCodes/UI_Sounds.cs
+++ Assets/Scripts/AuidoCodes/UI_Sounds.cs
@@ -11,6 +11,14 @@ public class UI_Sounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MakeClickSound();
+    }
+
+    void MakeClickSound()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SoundSystem.instance.PlaySound("click");
+        }
     }
 }
