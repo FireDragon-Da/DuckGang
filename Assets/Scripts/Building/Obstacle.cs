773e56fe06789ffc9aa01b2f56d1fddce2a3c880
SunilParab <sunilparab2023@gmail.com>
2026-03-04 11:33:19 -0500

    Added obstacles

---
+++ Assets/Scripts/Building/Obstacle.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Obstacle : Building
+{
+
+    void Start()
+    {
+        Build();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
