c7812d8e53eed6e3f190859c67e3cd822ebb1307
FireDragon-Da <honoryukun@gmail.com>
2026-03-09 22:38:19 -0700

    changes on duck stats and prefabs

--- Assets/Scripts/DuckBehaviour/DuckStats.cs
+++ Assets/Scripts/DuckBehaviour/DuckStats.cs
@@ -1,33 +1,51 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(DuckHunger))]
 public class DuckStats : MonoBehaviour
 {
+    private DuckHunger duckHunger;
+
+    [Header("Core Stats")]
     [SerializeField] private int happiness = 80;
-    [SerializeField] private int hunger = 60;
     [SerializeField] private int energy = 70;
     [SerializeField] private int health = 90;
 
-    public int Happiness => happiness;
-    public int Hunger => hunger;
-    public int Energy => energy;
-    public int Health => health;
-
-    public const int MaxStatValue = 100;
-
-    public void ModifyHappiness(int amount) => happiness = Mathf.Clamp(happiness + amount, 0, MaxStatValue);
-    public void ModifyHunger(int amount) => hunger = Mathf.Clamp(hunger + amount, 0, MaxStatValue);
-    public void ModifyEnergy(int amount) => energy = Mathf.Clamp(energy + amount, 0, MaxStatValue);
-    public void ModifyHealth(int amount) => health = Mathf.Clamp(health + amount, 0, MaxStatValue);
-
+    [Header("Lifespan Settings")]
     [SerializeField] float averageLifespan;
     [SerializeField] float lifespanVariance;
     float lifespan;
     float curLife;
 
+    public const int MaxStatValue = 100;
+
+    public int Happiness => happiness;
+    public int Energy => energy;
+    public int Health => health;
+
+    public int Hunger
+    {
+        get
+        {
+            if (duckHunger != null && duckHunger.MaxSatiety > 0)
+            {
+                return Mathf.RoundToInt((duckHunger.CurrentSatiety / duckHunger.MaxSatiety) * MaxStatValue);
+            }
+            return 0;
+        }
+    }
+
+    public int Age => Mathf.FloorToInt(curLife);
+    public int MaxAge => Mathf.FloorToInt(lifespan);
+
+    private void Awake()
+    {
+        duckHunger = GetComponent<DuckHunger>();
+    }
+
     void Start()
     {
-        lifespan = averageLifespan + UnityEngine.Random.Range(-lifespanVariance/2,lifespanVariance/2);
+        lifespan = averageLifespan + UnityEngine.Random.Range(-lifespanVariance / 2, lifespanVariance / 2);
     }
 
     void Update()
@@ -38,6 +56,10 @@ public class DuckStats : MonoBehaviour
             Die(DeathReason.OldAge);
         }
     }
+
+    public void ModifyHappiness(int amount) => happiness = Mathf.Clamp(happiness + amount, 0, MaxStatValue);
+    public void ModifyEnergy(int amount) => energy = Mathf.Clamp(energy + amount, 0, MaxStatValue);
+    public void ModifyHealth(int amount) => health = Mathf.Clamp(health + amount, 0, MaxStatValue);
 
     public void Die(DeathReason reason)
     {
