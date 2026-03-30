d1b551578ca59df31c19e53e9219b0beeb323c17
Henry Amato <henryamato77@gmail.com>
2026-03-30 13:40:04 -0400

    Update ArticleCreator
    - two structures: dictionary for specific articles, list for random fluff articles

--- Assets/Scripts/NewsAndText/ArticleCreator.cs
+++ Assets/Scripts/NewsAndText/ArticleCreator.cs
@@ -7,9 +7,15 @@ using UnityEngine;
 //this script should be attached to the gamemanager.
 //drag the TXT_Articles file (should be in the same folder) into the textFile field in the editor
 
+//the dictionary namedArticles contains all specific articles relating to game properties
+    //these can be accessed by their keys, ie StarvationArticle or FarmArticle
+//the list fluffArticles has all of the random articles that can be filled in in there are no named articles for the month
+    //these can just be added randomly since they won't correspond to anything
+
 public class ArticleCreator : MonoBehaviour
 {
-    public Dictionary<string, ArticleEvent> allArticles = new Dictionary<string, ArticleEvent>();
+    public Dictionary<string, ArticleEvent> namedArticles = new Dictionary<string, ArticleEvent>();
+    public List<ArticleEvent> fluffArticles = new List<ArticleEvent>();
     public TextAsset textFile;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -40,7 +46,14 @@ public class ArticleCreator : MonoBehaviour
 
         ArticleEvent newArticle = new ArticleEvent(data[1].Trim(), data[2].Trim(), data[3].Trim());
 
-        allArticles.Add(data[0], newArticle);
+        if (data[0].Trim() == "Fluff")
+        {
+            fluffArticles.Add(newArticle);
+        }
+        else
+        {
+            namedArticles.Add(data[0], newArticle);
+        }
     }
 
 }
