40e5f26811e528e8e02d0636487c1db47e987e44
Sunil Parab <sunilparab2023@gmail.com>
2026-05-09 18:59:06 -0400

    Fix for dragging animation triggers

--- Assets/Scripts/SelectionAndClick/OnClickDuck.cs
+++ Assets/Scripts/SelectionAndClick/OnClickDuck.cs
@@ -7,7 +7,6 @@ public class OnClickDuck : MonoBehaviour
 
     [SerializeField] private GameObject sliderPrefab;
     [SerializeField] private float sliderHeight = 50f;
-    private Canvas canvas;
 
     private DuckStats duckStats;
     private Camera mainCamera;
@@ -16,21 +15,19 @@ public class OnClickDuck : MonoBehaviour
     {
         duckStats = GetComponent<DuckStats>();
         mainCamera = Camera.main;
-        canvas = FindObjectOfType<Canvas>();
 
     }
 
     private void OnMouseDown()
     {
         duckStats.ModifyHappiness(10);
-        duckStats.SetDangling(true);
 
         //DuckStatDisplay.reference.displayStats(this.gameObject.GetComponent<DuckNameGen>().CurrentDuckName, duckStats);
     }
 
     private void OnMouseUp()
     {
-        duckStats.SetDangling(false);
+
     }
 
     private void OnMouseOver()
